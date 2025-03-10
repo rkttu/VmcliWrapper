d0febfaffe0e4c9952d6386e142a84a78833d1ff
Jung Hyun, Nam <rkttu@rkttu.com>
2025-03-10 17:35:55 +0900

    Add missing VirtualMachineEntryNames type

---
+++ src/VmcliWrapper/VirtualMachineEntryNames.cs
@@ -0,0 +1,44 @@
+﻿namespace VmcliWrapper;
+
+/// <summary>
+/// A collection of static readonly string fields that represent various entry names for a virtual machine configuration.
+/// </summary>
+public static class VirtualMachineEntryNames
+{
+    /// <summary>
+    /// Represents the configuration version entry name.
+    /// </summary>
+    public static readonly string Config_Version = "config.version";
+    /// <summary>
+    /// Represents the display name entry name.
+    /// </summary>
+    public static readonly string DisplayName = "displayName";
+    /// <summary>
+    /// Represents the guest operating system entry name.
+    /// </summary>
+    public static readonly string GuestOS = "guestOS";
+    /// <summary>
+    /// Represents the high precision event timer 0 present entry name.
+    /// </summary>
+    public static readonly string HighPrecisionEventTimer0_Present = "hpet0.present";
+    /// <summary>
+    /// Represents the maximum memory size entry name.
+    /// </summary>
+    public static readonly string Memory_MaxSize = "memory.maxsize";
+    /// <summary>
+    /// Represents the memory size entry name.
+    /// </summary>
+    public static readonly string MemorySize = "memsize";
+    /// <summary>
+    /// Represents the mouse, keyboard, and screen 3D enable entry name.
+    /// </summary>
+    public static readonly string MouseKeyboardScreen_Enable3d = "mks.enable3d";
+    /// <summary>
+    /// Represents the number of virtual CPUs entry name.
+    /// </summary>
+    public static readonly string NumberOfVirtualCPUs = "numvcpus";
+    /// <summary>
+    /// Represents the NVRAM entry name.
+    /// </summary>
+    public static readonly string NVRAM = "nvram";
+}
